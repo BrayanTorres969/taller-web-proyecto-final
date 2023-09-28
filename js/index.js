782630e798cd645b6c55d5f2aa7d590c8a288cf4
BrayanTorres <brayantorresacuna2023@gmail.com>
2023-09-28 13:54:15 -0500

    :hammer: Agregar funconalidad al botón open-menu

---
+++ js/index.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const menuToggle = document.querySelector(".open-menu");
+  const menu = document.querySelector(".menu");
+
+  menuToggle.addEventListener("click", function () {
+    menu.classList.toggle("active");
+    menuToggle.classList.toggle("active"); // Agregamos o eliminamos la clase "active" en el botón
+  });
+});
